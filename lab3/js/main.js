36cdde1883f1b429894ff03aa9207409592234b1
mason.y.koh <ko.terra.119@gmail.com>
2018-10-12 12:25:14 -0400

    lab3 done

--- lab3/js/main.js
+++ lab3/js/main.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: ___Youngmin Ko_____ Student ID: ___019155159__ Date: __OCT 12 2018 FRIDAY___
+*
+********************************************************************************/ 
+
 const urlString = "https://nameless-harbor-59273.herokuapp.com/";
 const VERBOSE = false;
 
@@ -114,6 +124,43 @@ function initializeProjects() {
     });
 }
 
+
+// send the updated team data to the correct route in the API
+function saveTeam() {
+    // Set the value of this to a local variable
+    // "this" in the context of this function will be a
+    // single observable "team" object from our viewModel.teams array
+    // - this is because this function is invoked from a "click"
+    //   binding from the view (index.html)
+    let currentTeam = this;
+    if (VERBOSE)
+      console.log(
+        "main.js:::saveTeam():::currentTeam.TeamName():::" +
+          currentTeam.TeamName()
+      );
+    $.ajax({
+      url: urlString + "team/" + currentTeam._id(),
+      type: "PUT",
+      data: JSON.stringify(
+        // create an object literal
+        {
+          Projects: currentTeam.Projects(),
+          Employees: currentTeam.Employees(),
+          TeamLead: currentTeam.TeamLead()
+        }
+      ),
+      contentType: "application/json"
+    })
+      .done(function(data) {
+        showGenericModal(
+          "Success",
+          "[" + currentTeam.TeamName() + "] Updated Successfully"
+        );
+      })
+      .fail(function(err) {
+        showGenericModal("Error", "Error updating the team information.");
+      });
+  }
 
 
 
