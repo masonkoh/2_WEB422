0c4f6a2ecad9ac79039d992d0c21d8df4a107332
mason.y.koh <ko.terra.119@gmail.com>
2018-11-02 20:55:25 -0400

    lab4 done

--- lab4/myapp/src/Navbar.js
+++ lab4/myapp/src/Navbar.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
+class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <a className="navbar-brand" href="/">WEB422 - Project Portal</a>
+                        <a className="navbar-brand" href="/">{this.props.title}</a>
                     </div>
                 </div>
             </nav>
-        )
+        );
     }
 }
+
+export default NavBar;
