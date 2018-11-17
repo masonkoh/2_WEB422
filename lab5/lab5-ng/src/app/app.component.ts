7a4016bbefd9ce4b35552bdea6f8a2a760df578d
mason.y.koh <ko.terra.119@gmail.com>
2018-11-16 23:54:16 -0500

    not everything work but have to submit

--- lab5/lab5-ng/src/app/app.component.ts
+++ lab5/lab5-ng/src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 05
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: ___Youngmin Ko___ Student ID: __019155159____ Date: __NOV.16.2018___
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
