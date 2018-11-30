fd1a5094fcb8348a340e08053da7eaf8634f8b15
mason.y.koh <ko.terra.119@gmail.com>
2018-11-30 16:49:00 -0500

    new path and componenet added

--- lab6/src/app/app-routing.module.ts
+++ lab6/src/app/app-routing.module.ts
@@ -6,12 +6,17 @@ import { EmployeesComponent } from './employees.component';
 import { PositionsComponent } from './positions.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
+import { EmployeeComponent } from './employee/employee.component';
+import { PositionComponent } from './position/position.component';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'employees', component: EmployeesComponent },
   { path: 'positions', component: PositionsComponent },
+  { path: 'employee/:_id', component: EmployeeComponent },
+  { path: 'position/:_id', component: PositionComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
