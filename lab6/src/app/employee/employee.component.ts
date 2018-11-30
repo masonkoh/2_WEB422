dba38d48a9644f04688bbaeca577cc2c313bcb41
mason.y.koh <ko.terra.119@gmail.com>
2018-11-30 16:27:29 -0500

    OnDestroy added

--- lab6/src/app/employee/employee.component.ts
+++ lab6/src/app/employee/employee.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EmployeeRaw } from '../data/employeeRaw';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../data/employee.service';
+import { PositionService } from '../position.service';
+import { Position } from '../data/position';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
+export class EmployeeComponent implements OnInit, OnDestroy {
 
   private paramSubScription: any;
   private employeeSubscription: any;
@@ -18,9 +22,39 @@ export class EmployeeComponent implements OnInit {
   private successMessage = false;
   private failMessage = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService, private positionService: PositionService) { }
 
   ngOnInit() {
+    this.paramSubScription = this.route.params.subscribe((params) => {
+      this.employeeSubscription = this.employeeService.getEmployee(params['_id']).subscribe((emp) => {
+        this.employee = emp[0];
+
+        this.getPositionsSub = this.positionService.getPositions().subscribe(data => {
+          this.positions = data;
+        });
+
+      });
+    });
+  }
+
+  onSubmit() {
+    this.saveEmployeeSubscription = this.employeeService.saveEmployee(this.employee).subscribe(() => {
+      this.successMessage = true;
+
+      // THERE WAS ONE LINE HERE BUT DELETE
+
+      setTimeout(() => {
+        this.successMessage = false;
+      }, 2500)
+
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.paramSubScription) { this.paramSubScription.unsubscribe(); }
+    if (this.employeeSubscription) { this.employeeSubscription.unsubscribe(); }
+    if (this.getPositionsSub) { this.getPositionsSub.unsubscribe(); }
+    if (this.saveEmployeeSubscription) { this.saveEmployeeSubscription.unsubscribe(); }
   }
 
 }
