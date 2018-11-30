db779d53ea9f3355354628d5d59349eb5ac4c670
mason.y.koh <ko.terra.119@gmail.com>
2018-11-30 16:06:48 -0500

    properties added

--- lab6/src/app/employee/employee.component.ts
+++ lab6/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeRaw } from '../data/employeeRaw';
 
 @Component({
   selector: 'app-employee',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+
+  private paramSubScription: any;
+  private employeeSubscription: any;
+  private getPositionsSubcription: any;
+  private getPositionsSub: any;
+  private saveEmployeeSubscription: any;
+  private employee: EmployeeRaw; // instruction wants me to fix it to non-private but it works. so i will keep it private anyways
+  private positions: Position[];
+  private successMessage = false;
+  private failMessage = false;
 
   constructor() { }
 
